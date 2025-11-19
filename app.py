cd57817847cf9c9b3f180d92b6b3d18b98731697
varun <varun@Ubuntu.myguest.virtualbox.org>
2025-11-19 14:22:41 +0530

    update message

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
-with open("version.txt", "r") as f:
-	version = f.read().strip()
+import time
+import subprocess
+import os
+import sys
 
-print(f"Hello Varun, this is version {version}")
+def restart_app():
+    """Restart the current Python script."""
+    print("Restarting app to apply updates...")
+    os.execv(sys.executable, [sys.executable] + sys.argv)
+
+print("Starting app...")
+print("Hello Varun, this is the current version of the app.")
+
+# Track last commit hash
+last_commit = subprocess.getoutput("git rev-parse HEAD")
+
+while True:
+    time.sleep(10)  # check every 10 seconds
+    subprocess.run(["git", "pull"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # Get current commit hash
+    current_commit = subprocess.getoutput("git rev-parse HEAD")
+    
+    if current_commit != last_commit:
+        print("New update detected from GitHub!")
+        last_commit = current_commit
+        restart_app()
